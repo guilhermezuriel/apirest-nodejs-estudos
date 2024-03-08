991cb572b1d296c9a3490073936c42cee8319967
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-08 19:30:37 -0300

    Criação de transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,26 @@
 import { FastifyInstance } from "fastify"
 import { kknex } from "../database"
+import {z} from 'zod'
+import { randomUUID } from "node:crypto";
 
 export async function transactionsRoutes(app: FastifyInstance){
-  app.get('/hello', async()=>{
-    const transactions = await kknex('transactions').where('amount', 1000).select('*')
-    return transactions
+  app.post('/', async(request, reply)=>{
+    //{title, amount, type}
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit'])
+    })
+
+    const {title, amount, type} =  createTransactionBodySchema.parse(request.body);
+
+    await kknex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount: amount*-1
+    })
+  
+    return reply.status(201).send()
 })
 
 }
--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,7 @@ import env from './env';
 import { transactionsRoutes } from './routes/transactions';
 const app = fastify();
 
-app.register(transactionsRoutes);
+app.register(transactionsRoutes,{
+    prefix:'transactions'
+});
 app.listen({port:env.PORT}).then(()=>console.log('HTTP Server Running!'))
