9de3c3d9aed76eb54704fb38c06d2d13beab3820
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-09 11:25:04 -0300

    Listagem de transações e transação única

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,25 @@ import {z} from 'zod'
 import { randomUUID } from "node:crypto";
 
 export async function transactionsRoutes(app: FastifyInstance){
+  app.get('/', async(request, reply)=>{
+    const transactions = await kknex('transactions').select('*')
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async(request, reply)=>{
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid()
+    })
+    const {id}  = getTransactionsParamsSchema.parse(request.params);
+
+    const transactions = await kknex('transactions').where('id',id).first()
+
+    return {transactions}
+  })
+
+
   app.post('/', async(request, reply)=>{
     //{title, amount, type}
     const createTransactionBodySchema = z.object({
@@ -19,7 +38,7 @@ export async function transactionsRoutes(app: FastifyInstance){
       title,
       amount: type === 'credit' ? amount: amount*-1
     })
-  
+   
     return reply.status(201).send()
 })
 
